dd88aa5abde5e576b305c4f9a375faabb0560734
Mufasir123 <62904284+Mufasir123@users.noreply.github.com>
2022-01-26 21:05:00 +0530

    Add files via upload

---
+++ Mufasir.js
@@ -0,0 +1,15 @@
+const email = prompt ("Entet your Email");
+const password = prompt ("Enter your password");
+
+/*if(email == password){
+				alert("Hurray! you logged in"); 
+}else{
+				alert("Try again");
+}*/
+switch(email){
+				case 1:alert("Hurray! you logged in");
+				break;
+				case 2:alert("Try again");
+				break;
+}
+
